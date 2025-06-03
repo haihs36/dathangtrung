459e58d3c7c05743b207d8be32c033c9672e3deb
root <root@server-103-74-122-126.da.direct>
2025-06-04 06:56:25 +0700

    update

---
+++ common/config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+    return [
+
+        'class' => 'yii\db\Connection',
+
+        'dsn' => 'mysql:host=127.0.0.1;dbname=dathangtru_db',
+
+        'emulatePrepare' => true,
+
+        'username' => 'dathangtru_db',
+
+        'password' => 'YLF7ZNd7jZPGeU3bQvXh',
+
+        'charset' => 'utf8',
+
+        'tablePrefix' => 'tb_',
+
+    ];
